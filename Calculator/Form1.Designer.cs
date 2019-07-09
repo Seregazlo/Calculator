9f338dd381cc20d77f0b1bc8370db0b6d0e9d691
Seregazlo <zlobin.sergey.00@mail.ru>
2019-07-09 12:28:17 +0600

    DesigningForm

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -28,20 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Addition = new System.Windows.Forms.Button();
+            this.Difference = new System.Windows.Forms.Button();
+            this.Multiplication = new System.Windows.Forms.Button();
+            this.Division = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(108, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(70, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(190, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "           ";
+            // 
+            // Addition
+            // 
+            this.Addition.Location = new System.Drawing.Point(12, 51);
+            this.Addition.Name = "Addition";
+            this.Addition.Size = new System.Drawing.Size(75, 23);
+            this.Addition.TabIndex = 3;
+            this.Addition.Text = "Сложение";
+            this.Addition.UseVisualStyleBackColor = true;
+            // 
+            // Difference
+            // 
+            this.Difference.Location = new System.Drawing.Point(103, 51);
+            this.Difference.Name = "Difference";
+            this.Difference.Size = new System.Drawing.Size(75, 23);
+            this.Difference.TabIndex = 4;
+            this.Difference.Text = "Вычитание";
+            this.Difference.UseVisualStyleBackColor = true;
+            // 
+            // Multiplication
+            // 
+            this.Multiplication.Location = new System.Drawing.Point(12, 100);
+            this.Multiplication.Name = "Multiplication";
+            this.Multiplication.Size = new System.Drawing.Size(75, 23);
+            this.Multiplication.TabIndex = 5;
+            this.Multiplication.Text = "Умножение";
+            this.Multiplication.UseVisualStyleBackColor = true;
+            // 
+            // Division
+            // 
+            this.Division.Location = new System.Drawing.Point(103, 100);
+            this.Division.Name = "Division";
+            this.Division.Size = new System.Drawing.Size(75, 23);
+            this.Division.TabIndex = 6;
+            this.Division.Text = "Деление";
+            this.Division.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(658, 436);
+            this.ClientSize = new System.Drawing.Size(242, 172);
+            this.Controls.Add(this.Division);
+            this.Controls.Add(this.Multiplication);
+            this.Controls.Add(this.Difference);
+            this.Controls.Add(this.Addition);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Addition;
+        private System.Windows.Forms.Button Difference;
+        private System.Windows.Forms.Button Multiplication;
+        private System.Windows.Forms.Button Division;
     }
 }
 
